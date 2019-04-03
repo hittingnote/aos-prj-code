ac1212564891b2a663a0154dc6cd940d4a1d4644
Administrator <admin@example.com>
2019-04-03 08:35:39 +0800

    default_pmm.c

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -142,7 +142,7 @@ default_alloc_pages(size_t n) {
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            SetPageProperty(p)
+            SetPageProperty(p);
             list_add_after(&free_list, &(p->page_link));
         }
         list_del(&(page->page_link));
