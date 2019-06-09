302167640c69170ef89fa38d40934babcf780ae1
hittingnote <hittingnote@163.com>
2019-06-09 17:06:11 +0800

    L6-initial

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -54,6 +54,17 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+	 
+	extern uintptr_t __vectors[];
+	int i;
+	for(i = 0; i < 256; i++)
+	{
+		SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+	}
+
+	SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
+	lidt(&idt_pd);
+
      /* LAB5 YOUR CODE */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
@@ -186,7 +197,7 @@ extern struct mm_struct *check_mm_struct;
 static void
 trap_dispatch(struct trapframe *tf) {
     char c;
-
+	struct trapframe switchk2u, *switchu2k;
     int ret=0;
 
     switch (tf->tf_trapno) {
@@ -220,6 +231,16 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+
+		ticks = ticks + 1;
+		if(ticks >= TICK_NUM)
+		{
+			ticks = 0;
+//			print_ticks();
+//			assert(current != NULL);
+			current->need_resched = 1;
+		}
+
         /* LAB5 YOUR CODE */
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
@@ -240,7 +261,28 @@ trap_dispatch(struct trapframe *tf) {
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
+        if(tf->tf_cs != USER_CS)
+        {
+            switchk2u = *tf;
+            switchk2u.tf_cs = USER_CS;
+            switchk2u.tf_ds = switchk2u.tf_es = switchk2u.tf_ss = USER_DS;
+            switchk2u.tf_esp = (uint32_t)tf + sizeof(struct trapframe) - 8;
+            
+            switchk2u.tf_eflags |= FL_IOPL_MASK;
+            
+            *((uint32_t*)tf-1) = (uint32_t)(&switchk2u);
+        }
+        break;
     case T_SWITCH_TOK:
+        if(tf->tf_cs != KERNEL_CS)
+        {
+            tf->tf_cs = KERNEL_CS;
+            tf->tf_ds = tf->tf_es = KERNEL_DS;
+            tf->tf_eflags &= ~FL_IOPL_MASK;
+            switchu2k = (struct trapframe*)(tf->tf_esp - (sizeof(struct trapframe)-8));
+            memmove(switchu2k, tf, sizeof(struct trapframe) - 8);
+            *((uint32_t*)tf-1) = (uint32_t)switchu2k;
+        }
         panic("T_SWITCH_** ??\n");
         break;
     case IRQ_OFFSET + IRQ_IDE1:
