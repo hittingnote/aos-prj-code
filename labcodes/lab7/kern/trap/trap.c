23a505fc08919cf59c193bdf81a7fd2175f535d0
Administrator <admin@example.com>
2019-06-15 09:32:21 +0800

    trap.c

--- labcodes/lab7/kern/trap/trap.c
+++ labcodes/lab7/kern/trap/trap.c
@@ -246,8 +246,9 @@ trap_dispatch(struct trapframe *tf) {
 	     * run_timer_list
          */
         ticks ++;
-        assert(current != NULL);
-        sched_class_proc_tick(current);
+//        assert(current != NULL);
+//        sched_class_proc_tick(current);
+        run_timer_list();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
